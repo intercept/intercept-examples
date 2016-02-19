9764068d5d2933770595020e2cbe80344fe97a1a
Nou <cliff.foster@idi-systems.com>
2016-02-19 01:30:32 -0800

    Added loader config addon for example dll.

---
+++ rv/example_dll/addons/example_dll/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches {
+    class example_dll {
+        units[] = {""};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = { "Intercept_Core" };
+        version = 0.1;
+    };
+};
+
+class Intercept {
+    class example_dll_project {
+        class example_dll_module {
+            pluginName = "example_dll";
+        };
+    };
+};
